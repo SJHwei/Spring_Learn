a68cfcb1252c9b4579153ca753ced4234e35b79d
SJHwei <2863254966@qq.com>
2021-04-01 17:00:07 +0800

    一些细微改变

--- day04_eesy_02account_aoptx_xml/src/test/java/com/itheima/test/AccountServiceTest.java
+++ day04_eesy_02account_aoptx_xml/src/test/java/com/itheima/test/AccountServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.List;
 
